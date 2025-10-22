f321d2aea577c7c107bde800b89a2dd553f4096d
Ondřej Zeman <ondrejzeman@zemn.xyz>
2025-10-22 05:20:43 +0200

    use custom dns

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -12,4 +12,10 @@
     trustedInterfaces = [ "tailscale0" ];
     allowedUDPPorts = [ 41641 ];
   };
+
+  environment.etc."systemd/resolved.conf.d/lab-dns.conf".text = ''
+		[Resolve]
+  	DNS=10.104.27.74
+  	Domains=~lab
+  '';
 }
