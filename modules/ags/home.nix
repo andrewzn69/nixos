2f5e5e6e3becae1dded7f3143f239305489e9f9d
Ondřej Zeman <ondrejzeman@zemn.xyz>
2025-12-26 02:56:57 +0100

    add AstalHyprland to extraPackages for workspace integration

--- modules/ags/home.nix
+++ modules/ags/home.nix
@@ -1,10 +1,13 @@
-{ ags, ... }:
+{ ags, pkgs, ... }:
 
 {
   imports = [ ags.homeManagerModules.default ];
 
   programs.ags = {
     enable = true;
+    extraPackages = [
+      ags.packages.${pkgs.system}.hyprland
+    ];
   };
 
   home.file.".config/ags" = {
