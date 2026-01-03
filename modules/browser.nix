e762f975bec431f5ad57628ce84095fffc4bbf24
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-03 22:32:07 +0100

    add ublock origin extension and disable warn on close

--- modules/browser.nix
+++ modules/browser.nix
@@ -1,10 +1,24 @@
-{ zen-browser, ... }:
+{
+  zen-browser,
+  firefox-addons,
+  pkgs,
+  ...
+}:
 
 {
   imports = [ zen-browser.homeModules.default ];
 
   programs.zen-browser = {
     enable = true;
-    # TODO: settings, plugins, theme
+
+    profiles.default = {
+      extensions.packages = with firefox-addons.packages.${pkgs.system}; [
+        ublock-origin
+      ];
+
+      settings = {
+        "browser.tabs.warnOnClose" = false;
+      };
+    };
   };
 }
