3388a281ad36695dcb2b400700573582017b8092
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-04 19:22:24 +0100

    configure extensions

--- modules/browser/default.nix
+++ modules/browser/default.nix
@@ -2,9 +2,13 @@
   zen-browser,
   firefox-addons,
   pkgs,
+  lib,
   ...
 }:
 
+let
+  extensions = import ./extensions.nix;
+in
 {
   imports = [ zen-browser.homeModules.default ];
 
@@ -14,7 +18,19 @@
     policies = import ./policies.nix;
 
     profiles.default = {
-      extensions = import ./extensions.nix { inherit firefox-addons pkgs; };
+      extensions = {
+        packages = builtins.map (ext: firefox-addons.packages.${pkgs.system}.${ext}) (
+          builtins.attrNames extensions
+        );
+        force = true;
+        settings = lib.mapAttrs' (
+          _name: value:
+          lib.nameValuePair value.id {
+            inherit (value) settings;
+            force = true;
+          }
+        ) (lib.filterAttrs (_n: v: v ? settings && v.settings != { }) extensions);
+      };
       settings = import ./settings.nix;
     };
   };
--- modules/browser/extensions.nix
+++ modules/browser/extensions.nix
@@ -1,12 +1,61 @@
-{ firefox-addons, pkgs }:
-
-let
-  myExtensions = with firefox-addons.packages.${pkgs.system}; [
-    ublock-origin
-    dearrow
-  ];
-in
 {
-  packages = myExtensions;
-  force = true;
+  ublock-origin = {
+    id = "uBlock0@raymondhill.net";
+    privateAllowed = true;
+    settings = {
+      selectedFilterLists = [
+        "user-filters"
+        "ublock-filters"
+        "ublock-badware"
+        "ublock-privacy"
+        "ublock-unbreak"
+        "ublock-quick-fixes"
+        "easylist"
+        "easyprivacy"
+        "urlhaus-1"
+        "plowe-0"
+      ];
+    };
+    permissions = [
+      "alarms"
+      "dns"
+      "menus"
+      "privacy"
+      "storage"
+      "tabs"
+      "unlimitedStorage"
+      "webNavigation"
+      "webRequest"
+      "webRequestBlocking"
+      "<all_urls>"
+      "http://*/*"
+      "https://*/*"
+      "file://*/*"
+      "https://easylist.to/*"
+      "https://*.fanboy.co.nz/*"
+      "https://filterlists.com/*"
+      "https://forums.lanik.us/*"
+      "https://github.com/*"
+      "https://*.github.io/*"
+      "https://github.com/uBlockOrigin/*"
+      "https://ublockorigin.github.io/*"
+      "https://*.reddit.com/r/uBlockOrigin/*"
+    ];
+  };
+  sponsorblock = {
+    id = "sponsorBlocker@ajay.app";
+    settings = {
+      alreadyInstalled = true;
+    };
+    permissions = [
+      "storage"
+      "scripting"
+      "https://sponsor.ajay.app/*"
+      "https://*.youtube.com/*"
+      "https://www.youtube-nocookie.com/embed/*"
+    ];
+  };
+  dearrow = {
+    id = "deArrow@ajay.app";
+  };
 }
