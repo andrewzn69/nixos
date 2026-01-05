56093f640bac62117426849ba3fb0662a0970e2a
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-05 12:43:46 +0100

    disable onboarding

--- modules/browser/settings.nix
+++ modules/browser/settings.nix
@@ -5,4 +5,13 @@
   "extensions.autoDisableScopes" = 0;
   "extensions.update.autoUpdateDefault" = false;
   "extensions.update.enabled" = false;
+
+  # disable onboarding
+  "browser.aboutwelcome.enabled" = false;
+  "trailhead.firstrun.didSeeAboutWelcome" = true;
+  "browser.startup.homepage_override.mstone" = "ignore";
+  "startup.homepage_welcome_url" = "";
+  "startup.homepage_override_url" = "";
+  "browser.messaging-system.whatsNewPanel.enabled" = false;
+  "browser.startup.firstrunSkipsHomepage" = true;
 }
