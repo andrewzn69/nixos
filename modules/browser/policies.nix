715a379994ad52757c644cafd039bf04bb695240
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-05 12:44:23 +0100

    update policies about privacy, telemetry and updates

--- modules/browser/policies.nix
+++ modules/browser/policies.nix
@@ -4,4 +4,27 @@
   };
 
   ExtensionRecommendations = false;
+
+  # privacy and telemetry
+  DisableTelemetry = true;
+  DisableFirefoxStudies = true;
+  DisablePocket = true;
+  DisableFirefoxAccounts = true;
+  DisableFeedbackCommands = true;
+
+  # updates
+  DisableAppUpdate = true;
+
+  # other privacy settings
+  DontCheckDefaultBrowser = true;
+  OverrideFirstRunPage = "";
+  OverridePostUpdatePage = "";
+
+  # tracking protection
+  EnableTrackingProtection = {
+    Value = true;
+    Locked = true;
+    Cryptomining = true;
+    Fingerprinting = true;
+  };
 }
