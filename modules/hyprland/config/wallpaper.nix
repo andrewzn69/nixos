fca90fe10027688dedb310971e031159f8f448de
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-06 11:50:12 +0100

    add missing directory to wallpaper

--- modules/hyprland/config/wallpaper.nix
+++ modules/hyprland/config/wallpaper.nix
@@ -5,10 +5,10 @@
     enable = true;
     settings = {
       preload = [
-        "$HOME/Pictures/wallpapers/TPR6JQ.jpg"
+        "$HOME/Pictures/wallpapers/gruvbox/TPR6JQ.jpg"
       ];
       wallpaper = [
-        "$HOME/Pictures/wallpapers/TPR6JQ.jpg"
+        "$HOME/Pictures/wallpapers/gruvbox/TPR6JQ.jpg"
       ];
     };
   };
