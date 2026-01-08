536a7e2c0fce4bb3be968b6bb819cb8c9824098c
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-08 18:13:56 +0100

    add misc settings file to hyprland module

---
+++ modules/hyprland/config/misc.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  wayland.windowManager.hyprland.settings = {
+    misc = {
+      disable_hyprland_logo = true;
+      disable_splash_rendering = true;
+      no_update_news = true;
+    };
+  };
+}
