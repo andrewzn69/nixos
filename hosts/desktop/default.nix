1a0bf20ff430e63c76d8c43e5a0b4f92e5ce25f5
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-29 19:25:39 +0100

    add audio module to desktop default.nix

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -18,7 +18,7 @@
 
     # desktop-specific
     ../../modules/hyprland
-    # ../../modules/audio.nix
+    ../../modules/audio.nix
     ../../modules/gaming.nix
     ../../modules/apps.nix
     ../../modules/fonts.nix
