8b6f8ac7ff8f5906264dece24624d67d48ce1f59
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-29 19:22:55 +0100

    add fontconfig for antialias, hinting and subpixel

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,11 +1,28 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    nerd-fonts.ubuntu
-    nerd-fonts.jetbrains-mono
-    noto-fonts-color-emoji
+  fonts = {
+    packages = with pkgs; [
+      nerd-fonts.ubuntu
+      nerd-fonts.jetbrains-mono
+      noto-fonts-color-emoji
+      freetype
+      # TODO: add CartographCF later
+    ];
 
-    # TODO: add CartographCF later
-  ];
+    fontconfig = {
+      enable = true;
+      antialias = true;
+
+      hinting = {
+        enable = true;
+        style = "slight";
+      };
+
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "default";
+      };
+    };
+  };
 }
