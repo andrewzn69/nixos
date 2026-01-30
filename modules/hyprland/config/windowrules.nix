f42c366711d24b62dd7bc34933d2eeed8d724079
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-01-30 02:53:00 +0100

    add value `on` to windowrules, change old effect names to new ones

--- modules/hyprland/config/windowrules.nix
+++ modules/hyprland/config/windowrules.nix
@@ -4,23 +4,23 @@
   wayland.windowManager.hyprland.settings = {
     windowrule = [
       # float
-      "match:class ^(org.kde.polkit-kde-authentication-agent-1)$, float"
-      "match:class ^(pavucontrol)$, float"
-      "match:class ^(file_progress)$, float"
-      "match:class ^(confirm)$, float"
-      "match:class ^(dialog)$, float"
-      "match:class ^(download)$, float"
-      "match:class ^(notification)$, float"
-      "match:class ^(error)$, float"
-      "match:class ^(confirmreset)$, float"
-      "match:title ^(Media viewer)$, float"
-      "match:title ^(Volume Control)$, float"
-      "match:title ^(Picture-in-Picture)$, float"
-      "match:title ^(DevTools)$, float"
-      "match:title ^(Open File)$, float"
-      "match:title ^(branchdialog)$, float"
-      "match:title ^(Confirm to replace files)$, float"
-      "match:title ^(File Operation Progress)$, float"
+      "match:class ^(org.kde.polkit-kde-authentication-agent-1)$, float on"
+      "match:class ^(pavucontrol)$, float on"
+      "match:class ^(file_progress)$, float on"
+      "match:class ^(confirm)$, float on"
+      "match:class ^(dialog)$, float on"
+      "match:class ^(download)$, float on"
+      "match:class ^(notification)$, float on"
+      "match:class ^(error)$, float on"
+      "match:class ^(confirmreset)$, float on"
+      "match:title ^(Media viewer)$, float on"
+      "match:title ^(Volume Control)$, float on"
+      "match:title ^(Picture-in-Picture)$, float on"
+      "match:title ^(DevTools)$, float on"
+      "match:title ^(Open File)$, float on"
+      "match:title ^(branchdialog)$, float on"
+      "match:title ^(Confirm to replace files)$, float on"
+      "match:title ^(File Operation Progress)$, float on"
 
       # position
       "match:title ^(Volume Control)$, move 75 44%"
@@ -43,9 +43,9 @@
 
       # XWaylandVideoBridge
       "match:class ^(xwaylandvideobridge)$, opacity 0.0 override 0.0 override"
-      "match:class ^(xwaylandvideobridge)$, noanim"
-      "match:class ^(xwaylandvideobridge)$, nofocus"
-      "match:class ^(xwaylandvideobridge)$, noinitialfocus"
+      "match:class ^(xwaylandvideobridge)$, no_anim on"
+      "match:class ^(xwaylandvideobridge)$, no_focus on"
+      "match:class ^(xwaylandvideobridge)$, no_initial_focus on"
     ];
   };
 }
