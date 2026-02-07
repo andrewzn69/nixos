ab36e869af536176413bbc98e4a89f9e8d47df0e
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-02-08 00:45:55 +0100

    add plugin list as comments for reference

--- modules/obsidian.nix
+++ modules/obsidian.nix
@@ -2,4 +2,30 @@
 
 {
   home.packages = [ pkgs.obsidian ];
+
+  # plugins to install manually (settings -> community plugins -> browse):
+  # - dataview
+  # - templater-obsidian
+  # - tag-wrangler
+  # - omnisearch
+  # - calendar
+  # - obsidian-projects
+  # - obsidian-vimrc-support
+  # - obsidian-git
+  # - obsidian-excalidraw-plugin
+  # - excalibrain
+  # - obsidian-plantuml
+  # - copy-document-as-html
+  # - obsidian-admonition
+  # - copy-image
+  # - image-converter
+  # - obsidian-linter
+  # - note-folder-autorename
+  # - remember-cursor-position
+  # - obsidian-scroll-offset
+  # - obsidian-style-settings
+  # - statusbar-organizer
+  # - settings-search
+  # - obsidian-auto-link-title
+  # - blockreffer
 }
