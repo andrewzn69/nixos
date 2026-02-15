cf76fa1de179e3a68144467ac5375ed3ebca844a
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-02-15 18:14:46 +0100

    fix cursor in fish shell

--- modules/fish/home.nix
+++ modules/fish/home.nix
@@ -3,6 +3,10 @@
 {
   programs.fish.enable = true;
 
+  # home-manager hardcodes KITTY_SHELL_INTEGRATION=no-rc, omitting no-cursor.
+  # this overrides it after the kitty block runs so __ksi_schedule skips cursor management.
+  programs.fish.shellInitLast = "set --global KITTY_SHELL_INTEGRATION 'no-rc no-cursor'";
+
   imports = [
     ./config/aliases.nix
     ./config/prompt.nix
