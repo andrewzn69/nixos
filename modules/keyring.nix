45787dcb8c00bbf8a18db7f5308c6e8ff54b917f
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-02-16 21:10:20 +0100

    fix gnome-keyring pam unlock for gdm

--- modules/keyring.nix
+++ modules/keyring.nix
@@ -2,5 +2,6 @@
 
 {
   services.gnome.gnome-keyring.enable = true;
-  security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.gdm.enableGnomeKeyring = true;
+  security.pam.services.gdm-password.enableGnomeKeyring = true;
 }
