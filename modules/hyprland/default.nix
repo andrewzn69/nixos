4a70b139c22ef2857f168fc4093733c927e83ae8
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-03-11 16:10:33 +0100

    force write hyprland portals conf with filechooser

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -16,6 +16,12 @@
     };
   };
 
+  environment.etc."xdg/xdg-desktop-portal/hyprland-portals.conf".text = ''
+    [preferred]
+    default=hyprland;gtk
+    org.freedesktop.impl.portal.FileChooser=gtk
+  '';
+
   environment.systemPackages = with pkgs; [
     hyprpaper
     hyprpicker
