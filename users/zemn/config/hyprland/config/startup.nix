fa8457914fddb783106813d035c9894d3ab58ca1
Ondřej Zeman <ondrejzeman@zemn.xyz>
2026-09-14 01:18:07 +0200

    remove ags

--- users/zemn/config/hyprland/config/startup.nix
+++ users/zemn/config/hyprland/config/startup.nix
@@ -8,7 +8,6 @@
       # "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       # "/usr/lib/polkit-kde-authentication-agent-1"
       # "systemctl --user restart pipewire"
-      "ags run"
       "systemctl --user restart hyprpaper"
       "[workspace 1 silent] zen-twilight"
       "[workspace 2 silent] beeper"
